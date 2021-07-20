664187c20e4738e1982b77c252a3740d0436c6a0
Vladimirsky <vladimir.bervin@gmail.com>
2021-07-20 23:56:41 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: 'AIzaSyD5u1w81hGur6TKAbQ1izDCI3KDaRwVYXo',
-  fbDbUrl: 'https://unification-books-default-rtdb.europe-west1.firebasedatabase.app'
+  apiKey: '',
+  fbDbUrl: ''
 };
